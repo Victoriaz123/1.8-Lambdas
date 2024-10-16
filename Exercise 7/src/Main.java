787c36cf1054e7b8678e1f2de2f2fd2b692ef8d2
Victoriaz123 <148845558+Victoriaz123@users.noreply.github.com>
2024-10-16 13:06:04 +0200

    Update Main.java

--- Exercise 7/src/Main.java
+++ Exercise 7/src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a list containing numbers and strings
+        
         List<Object> mixedList = Arrays.asList(
                 5, 10, "Fruits", "Vegetables", 20, "Berries", 15
         );
