affd8ea99d4ebfa26edd4cb5a50aba132703aa28
Victoriaz123 <148845558+Victoriaz123@users.noreply.github.com>
2024-10-24 11:22:25 +0200

    Update StringList.java

--- Exercise 1/src/StringList.java
+++ Exercise 1/src/StringList.java
@@ -5,7 +5,8 @@ public class StringList {
 
     public static List<String> filterStrings(List<String> strings) {
         return strings.stream()
-                .filter(s -> s.contains("o"))
+                .filter(s -> s.toLowerCase().contains("o"))
                 .collect(Collectors.toList());
     }
 }
+
