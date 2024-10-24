c90281b26b3ed8e1469ce89ed79d24a7d391dabb
Victoriaz123 <148845558+Victoriaz123@users.noreply.github.com>
2024-10-24 11:20:00 +0200

    Update Main.java

--- Exercise 1/src/Main.java
+++ Exercise 1/src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
 public static void main(String[] args) {
 
-    List<String> strings = Arrays.asList("one", "two", "three", "four", "five");
+    List<String> strings = Arrays.asList("One", "two", "three", "FOUR", "five");
     List<String> result = StringList.filterStrings(strings);
 
     System.out.println("Strings that contain a letter 'o': " + result);
